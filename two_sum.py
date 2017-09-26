0bf7c1a3e0ac1ef21c9f22d23b37bd53df842c3f
Valkure-Yip <yipchitung@hotmail.com>
2017-09-26 10:23:39 +0800

    1. Two Sum
    注意python中范围是不包括后阈值的
    [a:b]
    range(a,b)

--- two_sum.py
+++ two_sum.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for x in range(0,len(nums)-1):
+        for x in range(0,len(nums)-1):      #range(a,b)其实是[a,b)
             for y in range(x+1, len(nums)):
                 if nums[x]+nums[y]==target:
                     return [x,y]
